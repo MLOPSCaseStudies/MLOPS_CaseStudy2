c5aaf29abfbf73d8521c8839d80630f7d5bc852a
Shipra Poojary <poojaryshipra14@gmail.com>
2024-12-02 01:37:41 +0000

    test.

--- Azure_Build.sh
+++ Azure_Build.sh
@@ -12,7 +12,6 @@ az containerapp create \
     --resource-group CS553_CaseStudy4_group11 \
     --environment managedEnvironment-CS553CaseStudy4-9707 \
     --image poojary9991/cs553_casestudy4:latest \
-    --set-env-vars HF_API_KEY=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 OMDB=628cb164 \
     --ingress external \
     --target-port 7860
 
