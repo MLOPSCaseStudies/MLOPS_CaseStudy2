eacf026d389f171e668f23e41dc1542e8713915d
Shipra Poojary <54020226+poojary9991@users.noreply.github.com>
2024-12-01 23:27:32 -0500

    Update Azure_Build.sh

--- Azure_Build.sh
+++ Azure_Build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Build the container
-docker build -t poojary9991/cs553_casestudy4 .
-
+docker build --build-arg HF_API_KEY=$HF_API_KEY --build-arg OMDB=$OMDB -t poojary9991/cs553_casestudy4 .
 # Push the container to Docker Hub
 docker push poojary9991/cs553_casestudy4
 
@@ -16,7 +15,7 @@ az containerapp create \
     --image poojary9991/cs553_casestudy4:latest \  
     --ingress external \
     --target-port 7860
-    --env-vars HF_API_KEY=${HF_API_KEY} OMDB=${OMDB}
+    # --env-vars HF_API_KEY=${HF_API_KEY} OMDB=${OMDB}
 
 
 # Enable HTTPS Ingress
